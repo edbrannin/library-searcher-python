0a734d32ae2d371900f8eafe4bfdb2c9ab247dc6
Ed Brannin <edbrannin@gmail.com>
2016-02-20 18:19:53 -0500

    Much better.

--- render_results.py
+++ render_results.py
@@ -14,7 +14,8 @@ select
   r.title,
   h.branch_name,
   h.collection_name,
-  h.call_class
+  h.call_class,
+  count(*) as "count"
 from
   resources r,
   resource_holdings h,
@@ -25,10 +26,16 @@ where
   and s.item_identifier = h.barcode
   and position = 0
   and branch_name in (
-    -- 'Rochester Public Library Central',
+     'Rochester Public Library Central',
     'Irondequoit Public Library'
     )
   and available = 1
+group by
+  r.author,
+  r.title,
+  h.branch_name,
+  h.collection_name,
+  h.call_class
 order by
   author,
   title,
